98037c6c793cf50e3e3c7022a57bb1ef15b02811
Uilian Ries <uilianries@gmail.com>
2016-05-10 21:36:27 -0300

    Add process queue
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 
 #include <cstdlib>
 #include <regex>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
+#include <queue>
 
 #include <Poco/Logger.h>
 #include <Poco/AutoPtr.h>
@@ -21,6 +26,8 @@
 
 #include <IoT/MQTT/MQTTClient.h>
 #include <IoT/MQTT/MQTTClientFactory.h>
+
+#define RUN_ONLINE 0
 
 using IoT::MQTT::MQTTClientFactory;
 
@@ -59,24 +66,60 @@ class Target
 {
     /** Connect to data base */
     Poco::Data::Session sqlite_session_;
+    /** Lock message treatment */
+    std::mutex message_arrived_mutex_;
+
+    using message_queue = std::queue<IoT::MQTT::MessageArrivedEvent>;
+    message_queue message_queue_;
+    std::atomic_bool sentinel_;
+    std::unique_ptr<std::thread> message_arrived_consumer_;
+    std::condition_variable message_queue_condition_;
 public:
 
     Target()
     : sqlite_session_(Poco::Data::SessionFactory::instance().create("SQLite", "/var/www/html/db/SmartAquarium.sqlite"))
+    , sentinel_ { true }
     {
         Logger.instance().information("Connect on data base");
+        message_arrived_consumer_.reset(new std::thread(std::bind(&Target::ProcessMesssageQueue, this)));
+    }
+
+    ~Target()
+    {
+        sentinel_ = false;
+        message_queue_condition_.notify_all();
+        message_arrived_consumer_->join();
     }
 
     /**
-     * \brief Delegated function to treat arrived messages
-     * \param _sender Message provider
-     * \param _event Event released from server
+     * \brief Consumer loop
      */
-    void OnMessageArrived(const void* _sender, const IoT::MQTT::MessageArrivedEvent& _event)
+    void ProcessMesssageQueue()
     {
-        std::ignore = _sender;
+        while(sentinel_) {
+            std::unique_lock<std::mutex> lock{message_arrived_mutex_};
+            while (message_queue_.empty()) {
+                message_queue_condition_.wait(lock);
+                if (!sentinel_) {
+                    return;
+                }
+            }
+            Logger.instance().information("Process message queue");
+            auto message_event = message_queue_.front();
+            message_queue_.pop();
+            lock.unlock();
+            ProcessArrivedMessage(message_event);
+        }
+    }
 
-        std::regex expression(R"(^smartaquarium\/((\bsensor\b)|(\bactuator\b))\/(\w+)\/level$)");
+    /**
+     * \brief Process payload to insert on table
+     * \param event Data sent by broker
+     */
+    void ProcessArrivedMessage(const IoT::MQTT::MessageArrivedEvent& _event)
+    {
+        Logger.instance().information("Process arrived message");
+        const std::regex expression(R"(^smartaquarium\/((\bsensor\b)|(\bactuator\b))\/(\w+)\/level$)");
         std::smatch smatch;
         std::string topic{_event.topic};
         std::string table;
@@ -88,7 +131,6 @@ public:
         }
 
         std::ostringstream oss;
-        oss << __func__ << std::endl;
         oss << "Topic: " << _event.topic << std::endl;
         oss << "Payload: " << _event.message.payload << std::endl;
         oss << "Table: " << table << std::endl;
@@ -96,6 +138,22 @@ public:
 
         Poco::Data::Statement statement = (sqlite_session_ << "INSERT INTO " << table << " (value) VALUES (" << _event.message.payload << ")");
         statement.execute();
+        poco_assert (statement.done());
+    }
+
+    /**
+     * \brief Delegated function to treat arrived messages
+     * \param _event Event released from server
+     */
+    void OnMessageArrived(const void*, const IoT::MQTT::MessageArrivedEvent& _event)
+    {
+        std::lock_guard<std::mutex> lock(message_arrived_mutex_);
+        Logger.instance().information("On Message arrived");
+        auto wake = message_queue_.empty();
+        message_queue_.push(_event);
+        if ( wake ) {
+            message_queue_condition_.notify_one();
+        }
     }
 
     /**
@@ -130,10 +188,13 @@ protected:
     {
         MQTTClientFactory::FactoryArguments factoryArguments;
         factoryArguments.clientId = "WebSmartAquariumDeviceSupervisor";
+#if RUN_ONLINE
         factoryArguments.serverUri = "m11.cloudmqtt.com:15347";
         factoryArguments.options.username = "device_supervisor";
         factoryArguments.options.password = "devicesupervisor";
-
+#else
+        factoryArguments.serverUri = "localhost:1883";
+#endif
         client_ = MQTTClientFactory::CreateMQTTClient<MQTTClientFactory::ClientType::Paho>(factoryArguments);
 
         client_->connectionLost += Poco::delegate(&target_, &Target::OnConnectionLost);
